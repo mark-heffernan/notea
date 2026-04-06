7432f36cfe1e5504b13ad69df21c3e4cbdcef0eb
Mark Heffernan <42299523+mark-heffernan@users.noreply.github.com>
2026-04-06 15:34:43 +0100

    Add a home back link to the settings mobile screen

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -13,18 +13,30 @@ import { SSRContext, ssr } from 'libs/server/connect';
 import { applyReset } from 'libs/server/middlewares/reset';
 import { applyMisconfiguration } from 'libs/server/middlewares/misconfiguration';
 import { DebugInformation } from 'libs/shared/debugging';
+import UIState from 'libs/web/state/ui';
+import Link from 'next/link';
 
-const SettingsPage: NextPage<{ debugInformation: DebugInformation, tree: TreeModel }> = ({ tree, debugInformation }) => {
+const SettingsPage: NextPage<{
+    debugInformation: DebugInformation;
+    tree: TreeModel;
+}> = ({ tree, debugInformation }) => {
     const { t } = useI18n();
+    const { ua } = UIState.useContainer();
 
     return (
         <LayoutMain tree={tree}>
             <section className="py-40 h-full overflow-y-auto">
                 <div className="px-6 max-w-prose m-auto">
+                    {ua?.isMobileOnly && (
+                        <Link href="/" shallow>
+                            <a className="text-sm text-gray-500 hover:text-gray-800 flex items-center gap-1 mb-6 -mt-10">
+                                ← {t('Back')}
+                            </a>
+                        </Link>
+                    )}
                     <h1 className="font-normal text-4xl mb-10">
                         <span>{t('Settings')}</span>
                     </h1>
-
                     <SettingsContainer debugInfo={debugInformation} />
                     <SettingFooter />
                 </div>
